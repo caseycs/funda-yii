2768fb65bc7f76c2318f67eeaa05df22083759e9
Ilia Kondrashov <caseycs@gmail.com>
2014-06-19 02:29:08 +0400

    typo&path

--- app/extensions/MemCacheKeys.php
+++ app/extensions/MemCacheKeys.php
@@ -4,6 +4,6 @@
  */
 class MemCacheKeys
 {
-    const FUNDA_FETCH_REQUESTS_START = 'funda_fetch_start';
+    const FUNDA_FETCH_REQUESTS_START = 'funda_fetch_reqiests_start';
     const FUNDA_FETCH_REQUESTS_MADE = 'funda_fetch_requests_made';
 }
--- app/extensions/StdoutLogRoute.php
+++ app/extensions/StdoutLogRoute.php
@@ -4,6 +4,8 @@ class StdoutLogRoute extends CFileLogRoute
     public function init()
     {
         parent::init();
+        // this makes console loggings live - so we write to our log routes every time
+        // it is a bit slow, but makes easy to debug
         Yii::getLogger()->autoFlush = 1;
         Yii::getLogger()->autoDump = true;
     }
