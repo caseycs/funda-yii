be161e354645c0d3dfea4bb52b9c98dbbafdd9ec
Ilia Kondrashov <caseycs@gmail.com>
2014-06-19 02:25:53 +0400

    code organize

--- app/commands/FundaFetchCommand.php
+++ app/commands/FundaFetchCommand.php
@@ -16,6 +16,9 @@ class FundaFetchCommand extends CConsoleCommand
     /* @var CMemCache */
     private $CMemCache;
 
+    /* @var CDbConnection */
+    private $DB;
+
     public function run($args)
     {
         if ($this->rpm_limit < 2) throw new LogicException('rpm_limit lower then 2');
@@ -26,6 +29,7 @@ class FundaFetchCommand extends CConsoleCommand
         //dependency injection emulation - to change dependencies in test config
         $this->CMemCache = Yii::app()->cache;
         $this->FundaClient = Yii::app()->fundaClient;
+        $this->DB = Yii::app()->db;
 
         if ($this->initialFetchPagesCount()) {
             $this->updatePages();
@@ -53,28 +57,14 @@ class FundaFetchCommand extends CConsoleCommand
         return true;
     }
 
-    private function fetchPage(FundaFilter $FundaFilter, $page)
-    {
-        $options = array();
-        if ($FundaFilter->is_garden) {
-            $options[] = FundaClient::OPTION_GARDEN;
-        }
-
-        $result = $this->FundaClient->fetch(
-            $FundaFilter->location,
-            $FundaFilter->type,
-            $options,
-            $page
-        );
-        return $result;
-    }
-
     private function storePages(FundaFilter $FundaFilter, array $result)
     {
         if (!isset($result['Paging']['AantalPaginas'])) {
             throw new \Exception('Unexpected Funda output - paging');
         }
 
+        // actually $pages_count is very often incorrect - and realle there are less pages
+        // but we take this value and perform some blank requests to make our code bulletproof
         $pages_count = $result['Paging']['AantalPaginas'];
 
         for ($i = 1; $i <= $pages_count; $i ++) {
@@ -100,98 +90,6 @@ class FundaFetchCommand extends CConsoleCommand
         $FundaFilter->fetch_time = new CDbExpression('NOW()');
         if (!$FundaFilter->save()) {
             throw new \Exception('FundaFilter save fail');
-        }
-    }
-
-    private function updatePage(FundaPage $FundaPage, array $result)
-    {
-        if (!isset($result['Objects']) || !is_array($result['Objects'])) {
-            throw new \Exception('Unexpected Funda output - objects');
-        }
-
-        $realties_ids = array();
-
-        foreach ($result['Objects'] as $realty) {
-            if (!array_key_exists('GlobalId', $realty)
-                || !array_key_exists('MakelaarId', $realty)
-                || !array_key_exists('MakelaarNaam', $realty)
-            ) {
-                throw new \Exception('Unexpected Funda output - realty');
-            }
-
-            //agent
-            $Agent = Agent::model()->findByAttributes(array('MakelaarId' => $realty['MakelaarId']));
-            if (!$Agent) {
-                $Agent = new Agent;
-                $Agent->MakelaarId = (int)$realty['MakelaarId'];
-                $Agent->MakelaarNaam = $realty['MakelaarNaam'];
-            }
-            $Agent->update_time = new CDbExpression('NOW()');
-            $Agent->save();
-
-            //realty
-            $Realty = Realty::model()->findByAttributes(array('GlobalId' => $realty['GlobalId']));
-            if (!$Realty) {
-                $Realty = new Realty;
-                $Realty->GlobalId = $realty['GlobalId'];
-                $Realty->agent_id = $Agent->id;
-            }
-            $Realty->update_time = new CDbExpression('NOW()');
-            $Realty->save();
-
-            $realties_ids[] = $Realty->id;
-
-            //insert/update funda_page_realty_link
-            $command = Yii::app()->db->createCommand('INSERT INTO funda_page_realty_link
-                SET
-                    d_funda_filter_id = ' . $FundaPage->fundaFilter->id . ',
-                    funda_page_id = ' . $FundaPage->id . ',
-                    realty_id = ' . $Realty->id . '
-                ON DUPLICATE KEY UPDATE funda_page_id = ' . $FundaPage->id);
-            //Exception will be dropped here by CDbCommand
-            $command->execute();
-        }
-
-        //remove orphan rowsin funda_page_realty_link
-        $condition = 'd_funda_filter_id = ' . $FundaPage->fundaFilter->id
-            . ' AND funda_page_id = ' . $FundaPage->id;
-        if ($realties_ids) {
-            $condition .= ' AND realty_id NOT IN(' . join(',', $realties_ids) . ')';
-        }
-        //Exception will be also dropped here
-        Yii::app()->db->createCommand()->delete('funda_page_realty_link', $condition);
-
-        //finally update FundaPage
-        $FundaPage->fetch_time = new CDbExpression('NOW()');
-        $FundaPage->save();
-    }
-
-    /**
-     * To control how much requests per second this script makes to Funda we use 2 keys in memcache
-     * This is useful if we have few scripts, that use one API and they should split its RPM limit
-     */
-    private function isFundaRequestAvaliable()
-    {
-        $start = $this->CMemCache->get(MemCacheKeys::FUNDA_FETCH_REQUESTS_START);
-        if (!$start || $start < time() - 60) {
-            $this->CMemCache->set(MemCacheKeys::FUNDA_FETCH_REQUESTS_START, time());
-            $this->CMemCache->set(MemCacheKeys::FUNDA_FETCH_REQUESTS_MADE, 1);
-            Yii::log('Funda requests made set to 1');
-            return true;
-        }
-
-        $made = $this->CMemCache->get(MemCacheKeys::FUNDA_FETCH_REQUESTS_MADE);
-        if (!$made) {
-            $this->CMemCache->set(MemCacheKeys::FUNDA_FETCH_REQUESTS_MADE, 1);
-            Yii::log('Funda requests made set to 1');
-            return true;
-        } elseif ($made > $this->rpm_limit) {
-            Yii::log('Funda requests limit reached');
-            return false;
-        } else {
-            $this->CMemCache->memCache->increment(MemCacheKeys::FUNDA_FETCH_REQUESTS_MADE, 1);
-            Yii::log('Funda requests left: ' . ($this->rpm_limit - --$made));
-            return true;
         }
     }
 
@@ -235,5 +133,113 @@ class FundaFetchCommand extends CConsoleCommand
 
             $pages_left --;
         }
+    }
+
+    private function updatePage(FundaPage $FundaPage, array $result)
+    {
+        if (!isset($result['Objects']) || !is_array($result['Objects'])) {
+            throw new \Exception('Unexpected Funda output - objects');
+        }
+
+        $realties_ids = array();
+
+        foreach ($result['Objects'] as $realty) {
+            if (!array_key_exists('GlobalId', $realty)
+                || !array_key_exists('MakelaarId', $realty)
+                || !array_key_exists('MakelaarNaam', $realty)
+            ) {
+                throw new \Exception('Unexpected Funda output - realty');
+            }
+
+            //agent
+            $Agent = Agent::model()->findByAttributes(array('MakelaarId' => $realty['MakelaarId']));
+            if (!$Agent) {
+                $Agent = new Agent;
+                $Agent->MakelaarId = (int)$realty['MakelaarId'];
+                $Agent->MakelaarNaam = $realty['MakelaarNaam'];
+            }
+            $Agent->update_time = new CDbExpression('NOW()');
+            $Agent->save();
+
+            //realty
+            $Realty = Realty::model()->findByAttributes(array('GlobalId' => $realty['GlobalId']));
+            if (!$Realty) {
+                $Realty = new Realty;
+                $Realty->GlobalId = $realty['GlobalId'];
+                $Realty->agent_id = $Agent->id;
+            }
+            $Realty->update_time = new CDbExpression('NOW()');
+            $Realty->save();
+
+            $realties_ids[] = $Realty->id;
+
+            //insert/update funda_page_realty_link
+            $command = $this->DB->createCommand('INSERT INTO funda_page_realty_link
+                SET
+                    d_funda_filter_id = ' . $FundaPage->fundaFilter->id . ',
+                    funda_page_id = ' . $FundaPage->id . ',
+                    realty_id = ' . $Realty->id . '
+                ON DUPLICATE KEY UPDATE funda_page_id = ' . $FundaPage->id);
+            //Exception will be dropped here by CDbCommand
+            $command->execute();
+        }
+
+        //remove orphan rowsin funda_page_realty_link
+        $condition = 'd_funda_filter_id = ' . $FundaPage->fundaFilter->id
+            . ' AND funda_page_id = ' . $FundaPage->id;
+        if ($realties_ids) {
+            $condition .= ' AND realty_id NOT IN(' . join(',', $realties_ids) . ')';
+        }
+        //Exception will be also dropped here
+        $this->DB->createCommand()->delete('funda_page_realty_link', $condition);
+
+        //finally update FundaPage
+        $FundaPage->fetch_time = new CDbExpression('NOW()');
+        $FundaPage->save();
+    }
+
+    /**
+     * To control how much requests per second this script makes to Funda we use 2 keys in memcache
+     * This is useful if we have few scripts, that use one API and they should split its RPM limit
+     */
+    private function isFundaRequestAvaliable()
+    {
+        $start = $this->CMemCache->get(MemCacheKeys::FUNDA_FETCH_REQUESTS_START);
+        if (!$start || $start < time() - 60) {
+            $this->CMemCache->set(MemCacheKeys::FUNDA_FETCH_REQUESTS_START, time());
+            $this->CMemCache->set(MemCacheKeys::FUNDA_FETCH_REQUESTS_MADE, 1);
+            Yii::log('Funda requests made set to 1');
+            return true;
+        }
+
+        $made = $this->CMemCache->get(MemCacheKeys::FUNDA_FETCH_REQUESTS_MADE);
+        if (!$made) {
+            $this->CMemCache->set(MemCacheKeys::FUNDA_FETCH_REQUESTS_MADE, 1);
+            Yii::log('Funda requests made set to 1');
+            return true;
+        } elseif ($made > $this->rpm_limit) {
+            Yii::log('Funda requests limit reached');
+            return false;
+        } else {
+            $this->CMemCache->memCache->increment(MemCacheKeys::FUNDA_FETCH_REQUESTS_MADE, 1);
+            Yii::log('Funda requests left: ' . ($this->rpm_limit - --$made));
+            return true;
+        }
+    }
+
+    private function fetchPage(FundaFilter $FundaFilter, $page)
+    {
+        $options = array();
+        if ($FundaFilter->is_garden) {
+            $options[] = FundaClient::OPTION_GARDEN;
+        }
+
+        $result = $this->FundaClient->fetch(
+            $FundaFilter->location,
+            $FundaFilter->type,
+            $options,
+            $page
+        );
+        return $result;
     }
 }
