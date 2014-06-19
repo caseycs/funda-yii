1146ab59de75db467a9bd5e4500b99b539e49e6b
Ilia Kondrashov <caseycs@gmail.com>
2014-06-19 18:41:50 +0400

    organize

--- app/tests/bootstrap.php
+++ app/tests/bootstrap.php
@@ -3,6 +3,7 @@ $yiit = __DIR__ . '/../../vendor/yiisoft/yii/framework/yiit.php';
 $config = __DIR__ . '/../config/test.php';
 
 require $yiit;
+require __DIR__ . '/ReflectionHelper.php';
 require __DIR__ . '/WebTestCase.php';
 
 Yii::createWebApplication($config);
